c1cd65c98fc3ecea023a4caf3156312fa78dcea0
sbhtk <danielsaban2015@gmail.com>
2019-11-15 12:44:42 +0200

    tempoarary

--- app/src/main/java/com/example/myaudioplayer/MainActivity.java
+++ app/src/main/java/com/example/myaudioplayer/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myaudioplayer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Playlists.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
